852baebea75231242eccb5536d97b1d796328f96
Suhan Park <simon7233@gmail.com>
2020-03-25 21:51:09 -0400

    merch update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-<script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-</script>
  AOS.init({
  	duration: 800,
  	easing: 'slide'
