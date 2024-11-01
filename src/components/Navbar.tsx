086f5c03b745f77b15d4821bf347b814794e99d2
Delimaci <92929485+Delimaci@users.noreply.github.com>
2024-11-01 23:05:12 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 // components/Navbar.tsx
 
 import Link from "next/link";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
